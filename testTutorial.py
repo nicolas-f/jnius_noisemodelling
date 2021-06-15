8e1e6bb80cacbe4199814ae6f28f6c90672588e2
nicolas-f <[omitted]>
2021-06-15 11:33:45 +0200

    do sound propa

--- testTutorial.py
+++ testTutorial.py
@@ -1,9 +1,9 @@
 import jnius_config
 import os
+
 jnius_config.add_options('-Xmx4096m')
 jnius_config.set_classpath('dependencies/*')
 from jnius import autoclass
-
 
 # Imports
 SFSUtilities = autoclass('org.h2gis.utilities.SFSUtilities')
@@ -16,6 +16,9 @@ Import_File = autoclass('org.noise_planet.noisemodelling.wps.Import_and_Export.I
 LinkedHashMap = autoclass('java.util.LinkedHashMap')
 Sql = autoclass('groovy.sql.Sql')
 JDBCUtilities = autoclass('org.h2gis.utilities.JDBCUtilities')
+Regular_Grid = autoclass('org.noise_planet.noisemodelling.wps.Receivers.Regular_Grid')
+Noise_level_from_source = autoclass('org.noise_planet.noisemodelling.wps.NoiseModelling.Noise_level_from_source')
+Export_Table = autoclass('org.noise_planet.noisemodelling.wps.Import_and_Export.Export_Table')
 
 
 def to_groovy_map(obj):
@@ -29,12 +32,12 @@ def to_groovy_map(obj):
 
 
 def get_connection():
+    # load h2 database driver
     Driver.load()
-    # load H2GIS extension
-    connection = DriverManager.getConnection("jdbc:h2:"+os.path.abspath("localdb"),
-                    "sa", "sa")
-    # load h2 driver
+    connection = DriverManager.getConnection("jdbc:h2:" + os.path.abspath("localdb"),
+                                             "sa", "sa")
     if not JDBCUtilities.tableExists(connection, 'SPATIAL_REF_SYS'):
+        # load H2GIS extension
         H2GISFunctions.load(connection)
     connection = SFSUtilities.wrapConnection(connection)
     return connection
@@ -46,10 +49,25 @@ def main():
         sql = Sql(connection)
         Import_File().exec(connection, to_groovy_map({"pathFile": "data/buildings.shp"}))
         Import_File().exec(connection, to_groovy_map({"pathFile": "data/lw_roads.shp"}))
+
+        Regular_Grid().exec(connection, to_groovy_map({"sourcesTableName": "LW_ROADS",
+                                                       "delta": 50,
+                                                       "buildingTableName": "BUILDINGS"}))
+
+        print(Noise_level_from_source().exec(connection, to_groovy_map({"tableSources": "LW_ROADS",
+                                                                        "tableBuilding": "BUILDINGS",
+                                                                        "tableReceivers": "RECEIVERS",
+                                                                        "confReflOrder": 1,
+                                                                        "confDiffVertical": True,
+                                                                        "confDiffHorizontal": True,
+                                                                        "confSkipLevening": True,
+                                                                        "confSkipLnight": True,
+                                                                        "confSkipLden": True})))
+
+        Export_Table().exec(connection, to_groovy_map({"exportPath": "LDAY_GEOM.shp", "tableToExport": "LDAY_GEOM"}))
     finally:
         connection.close()
 
 
 if __name__ == '__main__':
     main()
-
